a4d38981d2d7973e15f21b9b849310c129442e40
emanuelgrotteria-debug <emanuelgrotteria@gmail.com>
2025-10-26 15:38:52 +0100

    Add files via upload

---
+++ W7D1 - Esercizio Pratica.sql
@@ -0,0 +1,108 @@
+USE AdventureWorksDW;
+#Esercizio1, Scrivi una query per verificare che il campo ProductKey nella tabella DimProduct sia una chiave primaria.
+
+SELECT DISTINCT ProductKey
+FROM dimproduct;
+SELECT ProductKey
+FROM dimproduct
+WHERE ProductKey IS NULL;
+SELECT 
+ProductKey,
+count(*)
+FROM dimproduct
+GROUP BY ProductKey
+HAVING count(*) > 1;
+
+#Esercizio2 #Scrivi una query per verificare che la combinazione dei campi SalesOrderNumber e SalesOrderLineNumber sia una PK.
+
+SELECT SalesOrderNumber,
+SalesOrderLineNumber,
+COUNT(*)
+FROM factresellersales
+GROUP BY 
+SalesOrderNumber,
+SalesOrderLineNumber
+HAVING COUNT(*) > 1;
+SELECT 
+SalesOrderNumber,
+SalesOrderLineNumber,
+COUNT(*)
+FROM factresellersales
+WHERE SalesOrderNumber IS NULL AND SalesOrderLineNumber IS NULL
+GROUP BY 
+SalesOrderNumber,
+SalesOrderLineNumber;
+
+#Esercizio3 Conta il numero transazioni (SalesOrderLineNumber) realizzate ogni giorno a partire dal 1 Gennaio 2020
+
+SELECT 
+    DATE(FR.OrderDate) AS Giorno,
+    COUNT(FR.SalesOrderLineNumber) AS NumeroTransazioni
+FROM factresellersales as FR
+WHERE FR.OrderDate >= '2020-01-01'
+GROUP BY DATE(FR.OrderDate)
+ORDER BY Giorno;
+
+#Esercizio4 Calcola il fatturato totale (FactResellerSales.SalesAmount), la quantità totale venduta 
+#(FactResellerSales.OrderQuantity) e il prezzo medio di vendita FactResellerSales.UnitPrice) per prodotto (DimProduct) 
+#a partire dal 1 Gennaio 2020. Il result set deve esporre pertanto il nome del prodotto, il fatturato totale, 
+#la quantità totale venduta e il prezzo medio di vendita. I campi in output devono essere parlanti!
+
+SELECT 
+P.EnglishProductName AS NomeProdotto,
+SUM(SalesAmount) AS FatturatoTotale,
+SUM(OrderQuantity) AS QuantitaTotaleVenduta,
+AVG(UnitPrice) AS PrezzoMedio,
+SUM(SalesAmount)/SUM(OrderQuantity) AS PrezzoMedio2
+FROM dimproduct AS P
+INNER JOIN
+factresellersales AS FR
+ON
+P.ProductKey = FR.ProductKey
+WHERE FR.OrderDate >= '2020-01-01'
+GROUP BY 
+P.EnglishProductName;
+
+#Esercizio5 Calcola il fatturato totale (FactResellerSales.SalesAmount) e la quantità totale venduta (FactResellerSales.OrderQuantity) 
+#per Categoria prodotto (DimProductCategory). Il result set deve esporre pertanto il nome della categoria prodotto,
+#il fatturato totale e la quantità totale venduta. I campi in output devono essere parlanti
+
+SELECT 
+C.EnglishProductCategoryName AS CategoriaProdotto,
+SUM(SalesAmount) AS FatturatoTotale,
+SUM(OrderQuantity) AS QuantitaTotaleVenduta,
+AVG(UnitPrice) AS PrezzoMedio
+FROM dimproduct AS P
+INNER JOIN 
+dimproductsubcategory AS S
+ON 
+P.ProductSubcategoryKey = S.ProductSubcategoryKey
+INNER JOIN 
+dimproductcategory AS C
+ON
+ S.ProductCategoryKey = C.ProductCategoryKey
+INNER JOIN
+factresellersales AS FR
+ON
+P.ProductKey = FR.ProductKey
+GROUP BY C.EnglishProductCategoryName;
+
+#Esercizio6 Calcola il fatturato totale per area città (DimGeography.City) realizzato a partire dal 1 Gennaio 2020.
+#Il result set deve esporre lʼelenco delle città con fatturato realizzato superiore a 60K
+
+SELECT 
+G.City AS Città,
+SUM(SalesAmount) AS FatturatoTotale
+FROM dimgeography AS G
+INNER JOIN 
+dimreseller AS R
+ON G.GeographyKey = R.GeographyKey
+INNER JOIN
+factresellersales AS FR
+ON 
+FR.ResellerKey = R.ResellerKey
+WHERE FR.OrderDate>= '2020-01-01'
+GROUP BY 
+G.CITY
+HAVING 
+SUM(SalesAmount) >60000;
